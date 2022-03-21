2beecff1f5411124d4660db05ba73fcecb8e7bc6
heekng <rhkd1769@gmail.com>
2022-03-22 00:39:24 +0900

    name 컬럼 추가

--- src/main/java/com/heekng/security/domain/Member.java
+++ src/main/java/com/heekng/security/domain/Member.java
@@ -13,13 +13,15 @@ public class Member {
     private Long memberId;
 
     private String id;
+    private String name;
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
-    public void createMember(String id, String password, Authority authority) {
+    public void createMember(String id, String password, String name, Authority authority) {
         this.id = id;
+        this.name = name;
         this.password = password;
         this.authority = authority;
     }
