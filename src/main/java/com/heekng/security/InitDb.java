2582c98a1845fc07acc31ca2a446b4fb42bbfc43
heekng <rhkd1769@gmail.com>
2022-03-22 00:35:52 +0900

    default 데이터 추가 코드 추가

---
+++ src/main/java/com/heekng/security/InitDb.java
@@ -0,0 +1,25 @@
+package com.heekng.security;
+
+import com.heekng.security.domain.Authority;
+import com.heekng.security.domain.Member;
+import com.heekng.security.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class InitDb {
+    private final MemberService memberService;
+
+    @PostConstruct
+    public void init() {
+        Member member = new Member();
+        member.createMember("heekng", "heekng1234", Authority.ROLE_ADMIN);
+        memberService.join(member);
+        log.info("create success!!");
+    }
+}
