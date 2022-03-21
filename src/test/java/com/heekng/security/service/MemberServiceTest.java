9de5e94e63cb7357515a37d8d04723a10455b8aa
heekng <rhkd1769@gmail.com>
2022-03-22 01:28:55 +0900

    회원가입 테스트코드 작성

---
+++ src/test/java/com/heekng/security/service/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package com.heekng.security.service;
+
+import com.heekng.security.domain.Authority;
+import com.heekng.security.domain.Member;
+import com.heekng.security.service.MemberService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.createMember("heekng2", "heekng1234", "고광2", Authority.ROLE_ADMIN);
+
+        //when
+        Long memberId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberService.findOne(memberId));
+    }
+}
